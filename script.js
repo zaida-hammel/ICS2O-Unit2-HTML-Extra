a82c597373bbec74b40691d54a12c7597b96c4a9
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-20 19:38:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Created by: Zaida Hammel
+// Created on: March 2022
+// This file contains the JS functions for index2.html
+
+/**
+ * Find area and perimeter.
+ */
+function doMathClicked() {
+  document.getElementById('area').innerHTML = '<p>The area is: ' + (5 * 3) + '</p>'
+  document.getElementById('perimeter').innerHTML = '<p>The perimeter is: ' + (5 + 3 + 5 + 3) + '</p>'
+}
